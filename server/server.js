39ebf24f0fc341e90a3f8cec8d47805e9640ae82
kspayne93 <kspayne93@gmail.com>
2019-03-07 01:50:28 -0700

    added endpoints and newsAPI calls to server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const NewsAPI = require('newsapi');
 require('dotenv').config();
 
 const app = express(); //Express server
 
 const { SERVER_PORT, NEWS_API_KEY } = process.env; //Environment Variables
+const newsapi = new NewsAPI(NEWS_API_KEY);
 
 app.listen(SERVER_PORT, () => {
    console.log(`Listening on port: ${SERVER_PORT}`)
@@ -15,6 +17,36 @@ app.use(express.json()); //JSON parser
 
 
 //ENDPOINTS
-app.get('/key', (req, res) => {
-   res.status(200).send(NEWS_API_KEY)
+//Get Trending Stories
+app.get('/topHeadlines/:page', async (req, res) => {
+   const { page } = req.params;
+   let articles = await newsapi.v2.topHeadlines({
+      category: 'technology',
+      language: 'en',
+      country: 'us',
+      page: `${page}`
+   })
+   res.status(200).send(articles)
+})
+
+//Get News Sources
+app.get('/sources', async (req, res) => {
+   let sources = await newsapi.v2.sources({
+      category: 'technology',
+      language: 'en',
+   })
+   res.status(200).send(sources)
+})
+
+//Search Query
+app.get('/search/', async (req, res) => {
+   const { q, page, sortBy } = req.query;
+   let searchResults = await newsapi.v2.everything({
+      q: `${q}`,
+      sources: 'ars-technica, crypto-coin-news, engadget, hackernews, recode, techcrunch, techradar, the-next-web, the-verge, wired',
+      language: 'en',
+      sortBy: `${sortBy}`,
+      page: `${page}`,
+   });
+   res.status(200).send(searchResults);
 })
